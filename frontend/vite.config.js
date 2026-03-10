741b5158be8f75caeaad907b8e46647ad21726ad
Fabricio Marcaccini <111884384+fabricioMarcaccini@users.noreply.github.com>
2026-03-10 17:08:33 -0300

    fix: update COOP to same-origin-allow-popups for Google Login

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -35,7 +35,7 @@ export default defineConfig({
     server: {
         port: 5173,
         headers: {
-            'Cross-Origin-Opener-Policy': 'unsafe-none',
+            'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
             'Cross-Origin-Embedder-Policy': 'unsafe-none',
         },
         proxy: {
