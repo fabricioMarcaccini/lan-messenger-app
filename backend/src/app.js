b454d636e35097837bd4745b5b1bc0e982ba25db
Fabricio Marcaccini <111884384+fabricioMarcaccini@users.noreply.github.com>
2026-03-11 16:16:57 -0300

    fix(google-login): fully default Cross-Origin-Opener-Policy to unsafe-none across stack

--- backend/src/app.js
+++ backend/src/app.js
@@ -158,7 +158,7 @@ app.use(koaBody({
 app.use(helmet({
     crossOriginResourcePolicy: false,
     crossOriginEmbedderPolicy: false,
-    crossOriginOpenerPolicy: { policy: 'same-origin-allow-popups' },
+    crossOriginOpenerPolicy: { policy: 'unsafe-none' },
     contentSecurityPolicy: false,
 }));
 
